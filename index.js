c2835f12152563fddf080b3480cc9a562f756780
Bruno Cruz <brunolvcruz@gmail.com>
2020-09-03 21:27:28 -0300

    index.js com configuração de porta dinamica para heroku

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(cors()); //LIBERANDO TODOS ENDPOINT PARA ACESSO DE OUTROS DOMINIOS
 
 app.use("/account", account);
 
-app.listen(3003, async () => {
+app.listen(process.env.PORT || 8080, async () => {
   try {
     logger.info("API Started");
   } catch (error) {
